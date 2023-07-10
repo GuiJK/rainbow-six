8817ada613a068b324a7d44da743766537e589ff
GuiJK <junquekarabe18@gmail.com>
2023-07-10 13:02:18 -0300

    upload de projeto

--- src/js/index.js
+++ src/js/index.js
@@ -18,53 +18,53 @@
 // OBJETIVO 1 - quando passar o mouse em cima do personagem na listagem, devemos selecioná-lo
 // passo 1 - pegar os personagens no JS pra poder verificar quando o usuário passar o mouse em cima de um deles
 
-const personagens = document.querySelectorAll('.personagem');
+const operadores = document.querySelectorAll('.operador');
 
 
 // passo 2 - adicionar a classe selecionado no personagem que o usuário passar o cursor do mouse
-personagens.forEach((personagem) => {
-  personagem.addEventListener('mouseenter', () => {
+operadores.forEach((operador) => {
+  operador.addEventListener('mouseenter', () => {
 
     if (window.innerWidth < 450) {
       window.scrollTo({ top: 0, behavior: 'smooth' });
     }
 
     // passo 3 - verificar se já existe um personagem selecionado, se sim, devemos remover a seleção dele
-    removerSelecaoDoPersonagem();
+    removerSelecaoDoOperador();
 
-    personagem.classList.add('selecionado');
+    operador.classList.add('selecionado');
 
     // OBJETIVO 2 - quando passar o mouse em cima do personagem na listagem, trocar a imagem, o nome e a descrição do personagem grande
 
     //passo 1 - pegar o elemento do personagem grande pra adicionar as informações nele
-    alterarImagemPersonagemSelecionado(personagem);
+    alterarImagemOperadorSelecionado(operador);
 
     // passo 3 - alterar o nome do personagem grande
-    alterarNomePersonagemSelecionado(personagem);
+    alterarNomeOperadorSelecionado(operador);
 
     // passo 4 - alterar a descrição do personagem grande
-    alterarDescricaoPersonagem(personagem);
+    alterarDescricaoOperador(operador);
   })
 })
 
-function alterarDescricaoPersonagem(personagem) {
-  const descricaoPersonagem = document.getElementById('descricao-personagem');
-  descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+function alterarDescricaoOperador(operador) {
+  const descricaoOperador = document.getElementById('descricao-operador');
+  descricaoOperador.innerText = operador.getAttribute('data-description');
 }
 
-function alterarNomePersonagemSelecionado(personagem) {
-  const nomePersonagem = document.getElementById('nome-personagem');
-  nomePersonagem.innerText = personagem.getAttribute('data-name');
+function alterarNomeOperadorSelecionado(operador) {
+  const nomeOperador = document.getElementById('nome-operador');
+  nomeOperador.innerText = operador.getAttribute('data-name');
 }
 
-function alterarImagemPersonagemSelecionado(personagem) {
-  const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+function alterarImagemOperadorSelecionado(operador) {
+  const imagemOperadorGrande = document.querySelector('.operador-grande');
   // passo 2 - alterar a imagem do personagem grande
-  const idPersonagem = personagem.attributes.id.value;
-  imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+  const idOperador = operador.attributes.id.value;
+  imagemOperadorGrande.src = `./src/imagens/${idOperador}.png`;
 }
 
-function removerSelecaoDoPersonagem() {
-  const personagemSelecionado = document.querySelector('.selecionado');
-  personagemSelecionado.classList.remove('selecionado');
+function removerSelecaoDoOperador() {
+  const operadorSelecionado = document.querySelector('.selecionado');
+  operadorSelecionado.classList.remove('selecionado');
 }
